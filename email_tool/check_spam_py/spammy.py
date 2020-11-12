3ef952f19545739c46a18a85f2fd70086f29b18f
Larry Montgomery <larry.montgomery2091@gmail.com>
2020-11-12 01:45:34 -0800

    split the function get_all_email into a report and json parsing functions

--- email_tool/check_spam_py/spammy.py
+++ email_tool/check_spam_py/spammy.py
@@ -29,39 +29,55 @@ def get_users():
         if check_user.search(user) == None:
             valid_users.append(user)            
     return valid_users
+#
+# Method takes list of users and returns list of email accounts
+# by parsing the Json in email_accounts.json 
+# Returns a dictionary of domain
+#
+def parse_cp_email(jsonFile):
+    acct_map = {}
+    domain_usr = json.load(jsonFile)
+    for acct in domain_usr:
+        if acct != "__version":
+            type(domain_usr.get(acct).get('accounts').keys())
+            acct_map[acct] = domain_usr.get(acct).get('accounts').keys()
+        else:
+            continue
+    return acct_map        
 
-#  
+def report_all_email(data):
+    for user in data.keys():
+        print(f'{bcolors.CYAN}The user {user} has: \n {bcolors.ENDC}')
+        if data.get(user) == None:
+            print(f'{bcolors.RED} No Email Account {bcolors.ENDC}')            
+        else:
+            for domain in data.get(user).keys():
+                tmpData = data.get(user).get(domain)
+                if tmpData == {}:
+                    print(f'{bcolors.RED} {domain} has No Email Accounts')
+                else:
+                    print(f'{bcolors.YELLOW} \t {domain} has: \n {bcolors.ENDC}')
+                    for each in tmpData:
+                        print(f'{bcolors.GREEN} \t\t{each}@{domain} \n {bcolors.ENDC}')
+
+# 
 # Method parses email_accounts.json file for each user 
 # and lists the email accounts per domain.
 #
 def get_email_accounts():
+    data = {}
     users = get_users()
     for user in users:
          email_file = f'/home/{user}/.cpanel/email_accounts.json'
          if os.path.getsize(email_file) != 0:
             with open(email_file) as f:
-                try:
-                    print(f'{bcolors.CYAN}The user {user} has:{bcolors.ENDC}')
-                    e_domains_usr = json.load(f)
-                    for acct  in e_domains_usr:
-                        if acct != "__version":
-                            e_data = e_domains_usr.get(acct)
-                            if e_data.get('account_count') == 0:
-                                print (f"{bcolors.RED} Domain {acct} has no email accounts{bcolors.ENDC} \n")
-                            else:
-                                print (f"\t{bcolors.YELLOW} Domain {acct} has: \n {bcolors.ENDC}")
-                                for each in e_data.get("accounts"):
-                                    print (f'\t\t{bcolors.GREEN}{each}@{acct} \n {bcolors.ENDC}')
-                        else:
-                            pass
-                        #print(f'{bcolors.GREEN}{acct}{bcolors.ENDC}')
-                    #print (json.dumps(e_domains_usr, indent=4, sort_keys=True))
-                except Exception as e:
-                    print (e)
+                user_data = parse_cp_email(f)
+                data[user] = user_data
+                
          else:
-            print(f'''{bcolors.CYAN}The user {user} has: \n  
-            {bcolors.ENDC} {bcolors.RED} No Email Account {bcolors.ENDC}''')            
-# 
+            print(f'{bcolors.CYAN}The user {user} has: \n {bcolors.ENDC} {bcolors.RED} No Email Account {bcolors.ENDC}')            
+    report_all_email(data)
+#   
 # Method prints the contents of config files for email routing
 #
 #
@@ -95,20 +111,22 @@ def general_exim_parser():
 
 def generate_help():
      parser = argparse.ArgumentParser(prog='Spammy',description='Spammy is the spam assasin we deserve')
-     parser.add_argument('-a', help='This message shows the same information')
-     parser.add_argument('--check_routing', help='prints the email routing for all domains')
-     parser.add_argument('--user', help='This will show all email for a email user')
-     parser.add_argument('--since', help='shows email totals from time frame provided until present')
-     parser.add_argument('--from', help='select emails with specific FROM values')
+     parser.add_argument('-a','--all_email',action='store_true',
+     help='This message shows the same information')
+     parser.add_argument('-r','--email_routing', action='store_true', 
+     help='prints the email routing for all domains')
+     #parser.add_argument('--user', help='This will show all email for a email user')
+     #parser.add_argument('--since', help='shows email totals from time frame provided until present')
+     #parser.add_argument('--from', help='select emails with specific FROM values')
      args = parser.parse_args()
      return args
 
 def main():
    args = generate_help()
    print (args)
-   if args.a == "5":
+   if args.all_email == True:
        get_email_accounts()
-   if args.check_routing == "1":
+   if args.email_routing == True:
        get_email_routing()
 
 if __name__ == "__main__":
